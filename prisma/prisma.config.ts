7e9dd1ebfafda1ca9e616ed71ada5419800fa7fc
Peter <thegreatsidhu@gmail.com>
2026-06-18 16:27:31 -0700

    Fix prisma config

--- prisma/prisma.config.ts
+++
@@ -1,4 +0,0 @@
-import { defineConfig } from "prisma/config";
-export default defineConfig({
-  datasource: { url: "postgresql://neondb_owner:npg_62VwRJYymNzk@ep-curly-forest-ah0d1t54.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require" },
-});
